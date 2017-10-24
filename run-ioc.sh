9e9f2e96c985e35c87af2a91e069befd991eed82
Wesley Moore <wmoore@jlab.org>
2017-10-24 10:14:48 -0400

    run-ioc.sh: updated to rebuild apps and use parameters for ioc/port

--- run-ioc.sh
+++ run-ioc.sh
@@ -1,8 +1,19 @@
-#!/bin/sh
+#!/bin/bash
 
-IOCNAME=iocmicroEpsilon
+echo "Checking for updates to apps..."
+cd /home/epics/apps
+git pull origin master
+if [ $? != 0 ]
+then
+    make
+fi
 
-cd /home/epics/apps/iocBoot/$IOCNAME
-#git pull origin master
-procServ -f -P 20000 -L /logs/$IOCNAME.log ./st.cmd
-
+if [ $# -gt 0 ]; then
+    ioc=$1
+    port=$2
+    cd iocBoot/$ioc
+    echo "Starting $ioc on port $port..."
+    procServ -f -P $port -L /logs/$ioc.log --logstamp ./st.cmd
+else
+    echo "No ioc specified. Example: docker run -e ioc=iocmicroEspilon -e port=20000 -d wmoore28/rpi-epics-hallc"
+fi
